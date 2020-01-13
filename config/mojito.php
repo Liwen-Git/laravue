5b42637f6e58629d43786e2138280eababd01b1d
liwen <liwen1010322212@163.com>
2020-01-13 16:07:18 +0800

    后台权限 路由 梳理

--- config/mojito.php
+++ config/mojito.php
@@ -9,10 +9,13 @@ return [
     'passport_refresh_token_ttl' => env('PASSPORT_REFRESH_TOKEN_TTL', 7),
 
     'super_admin' => [
+        //  后台用户的 provider，登录时需要
         'provider' => env('MOJITO_SUPER_ADMIN_PROVIDER', 'admin'),
 
+        // 后台用户的鉴权中间件
         'auth' => env('MOJITO_SUPER_ADMIN_AUTH', 'auth:admin'),
 
+        // 后台用户的 guard
         'guard' => env('MOJITO_SUPER_ADMIN_GRARD', 'admin')
     ],
 
--- resources/js/config/index.js
+++ resources/js/config/index.js
@@ -4,12 +4,16 @@ export default {
             clientId: 2,
             clientSecret: '5GEuFn30gM0sEW1QdhknBzoctqIgFgyRzRQq6rTc'
         },
+        // provider 为 admin 时登录路由名
         loginRouteName: 'adminLogin',
 
+        // provider 为 admin 初始页面路由
         dashboardName: 'adminDashboard',
 
+        // provider 为 admin 初始页面完全路径
         dashboardFullPath: '/admin/dashboard',
 
+        // 项目全名 和 项目缩写名
         appName: {
             fullName: '多乐后台管理',
             abbrName: '多乐'
@@ -18,6 +22,7 @@ export default {
         locale: 'zh'
     },
 
+    // 支持多表登录时，guard 键值对
     guardNames: [
         {
             label: 'admin',
